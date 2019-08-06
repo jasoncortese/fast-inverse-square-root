b7d873743345cf61d285e8c56119578fe152f1b7
jasoncortese <jasoncort@gmail.com>
2019-08-06 16:55:32 -0700

    Update inheritance.js

--- inheritance.js
+++ inheritance.js
@@ -1,30 +1,143 @@
-class Pet {
-    constructor(legs = 0) {
-        this.legs = legs;
-    }
-    
-    says(sound = '') {
-        external.play(sound);
+class Boat {
+    constructor() {
+        this.length = 10;
+        this.distance = 0;
     }
 }
 
-class Dog extends Pet {
-    constructor(legs = 4) {
-        super(legs);
+class SailBoat extends Boat {
+    constructor() {
+        super();
     }
     
-    says(sound = 'bark') {
-        super.says(sound);
+    sail() {
+        this.distance += this.length;
     }
 }
 
-class LoudDog extends Dog {
-    constructor(legs) {
-        super(legs);
+class LongSailBoat extends SailBoat {
+    constructor(length) {
+        super(length);
+        this.length *= 2;
     }
     
-    says(sound, volume = 10) {
-        external.valume = volume;
-        super.says(sound);
+    sail() {
+        super.sail();
+    }
+}
+
+class MotorBoat extends Boat {
+    constructor() {
+        super();
+    }
+    
+    motor() {
+        this.distance += this.length * 3;
+    }
+}
+
+class FastMotorBoat extends Boat {
+    constructor() {
+        super();
+    }
+    
+    motor() {
+        this.distance += this.length * 5;
+    }
+}
+
+class FasterMotorBoat extends Boat {
+    constructor() {
+        super();
+    }
+    
+    motor() {
+        this.distance += this.length * 7;
+    }
+}
+
+class Boat {
+    constructor() {
+        this.length = 10;
+        this.distance = 0;
+        this.speed = 1;
+    }
+}
+
+class MotorBoat extends Boat {
+    constructor() {
+        super();
+        this.speed = 3;
+    }
+    
+    motor() {
+        this.distance += this.length * this.speed;
+    }
+}
+
+class FastMotorBoat extends MotorBoat {
+    constructor() {
+        super();
+        this.speed = 5;
+    }
+}
+
+class FastSailBoat extends SailBoat {
+    constructor() {
+        super();
+        this.speed = 2;
+    }
+}
+
+class DieselMotorBoat extends MotorBoat {
+    constructor() {
+        super();
+        this.fuel = 'diesel';
+    }
+    
+    motor() {
+        super.motor();
+    }
+}
+
+class Boat {
+    constructor() {
+        this.length = 10;
+        this.distance = 0;
+        this.speed = 1;
+        this.fuel = '';
+    }
+}
+
+class FastDieselMotorBoat extends SailBoat {
+    constructor() {
+        super();
+    }
+    
+    motor() {
+        this.distance += this.length * 5;
+    }
+}
+
+class LongFastDieselMotorBoatWithSail extends LongSailBoat {
+    constructor() {
+        super();
+        this.speed = 5;
+        this.fuel = 'diesel';
+    }
+    
+    motor() {
+        this.distance += this.length * this.speed;
+    }
+}
+
+class LongFastDieselMotorBoatWithSail extends FastDieselMotorBoat {
+    constructor() {
+        super();
+        this.length *= 2;
+    }
+    
+    sail() {
+        this.distance += this.length;
     }
 }
