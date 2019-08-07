94e8dfa40fa06e086e583e7514ffd613a82296a5
jasoncortese <jasoncort@gmail.com>
2019-08-07 10:52:37 -0700

    Create state-object.js

---
+++ state-object.js
@@ -0,0 +1,59 @@
+class State {
+    constructor() {
+    }
+    
+    get() {}
+    
+    set() {}
+}
+
+class UndoableState extends State {
+    constructor() {
+    }
+    
+    undo() {}
+    
+    redo() {}
+}
+
+
+class LockableState extends State {
+    constructor() {
+    }
+    
+    lock() {}
+    
+    unlock() {}
+}
+
+class UndoableState extends State {
+    constructor() {
+        new Undoable(this);
+    }
+}
+
+class Undoable extends Aggregate {
+    undo() {}
+    
+    redo() {}
+}
+
+class LockableState extends State {
+    constructor() {
+        new Lockable(this);
+    }
+}
+
+class Lockable extends Aggregate {
+    lock() {}
+    
+    unlock() {}
+}
+
+
+class UndoableLockableState extends State {
+    constructor() {
+        new Undoable(this);
+        new Lockable(this);
+    }
+}
